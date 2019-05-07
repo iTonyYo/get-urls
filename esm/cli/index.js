23ae6f0dfc3762511acc2ed631d1bc02ce882c30
Tony Stark <swzyocowboy@icloud.com>
2019-05-07 19:57:06 +0800

    重构: :recycle: 将 CUI 中的错误处理隔离看待，独立于主要逻辑之外

--- esm/cli/index.js
+++ esm/cli/index.js
@@ -25,78 +25,82 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 // 待办： 支持直接在命令行界面中提供一段文本并从中获取所有链接
 // 待办： 若未提供 `out` 参数，则直接在命令行界面中输出结果
+const start = async () => {
+  const cli = (0, _meow.default)(`
+    使用方式
+      $ get-urls <文件> <...> 选项 [...]
+
+    选项
+      --out, -o, 指定存储位置，默认：'process.cwd()'，包含文件名，将获取到的链接以文件的形式保存
+      --exclude, -f, 指定一个或多个过滤规则
+      --extractFromQueryString, 是否从找到的链接中提取作为查询参数内容链接
+      --version, -V, 查看版本号
+
+    示例
+      $ get-urls ./playlist.m3u -o ./test.txt -f '(s|jpg)'
+  `, {
+    flags: {
+      exclude: {
+        type: 'string',
+        alias: 'f'
+      },
+      out: {
+        type: 'string',
+        alias: 'o'
+      },
+      extractFromQueryString: {
+        type: 'boolean'
+      },
+      help: {
+        type: 'boolean',
+        alias: 'h'
+      },
+      version: {
+        type: 'boolean',
+        alias: 'V'
+      }
+    }
+  });
+  (0, _updateNotifier.default)({
+    pkg: cli.pkg
+  }).notify();
+  const {
+    input,
+    flags
+  } = cli;
+  const {
+    out,
+    exclude,
+    extractFromQueryString
+  } = flags;
+  const noInput = (0, _isEqual.default)((0, _size.default)(input), 0);
+  const onlyOneInput = (0, _isEqual.default)((0, _size.default)(input), 1);
+  const hasMutipleInput = (0, _size.default)(input) > 1;
+
+  if (noInput) {
+    throw Error('必须提供源文件');
+  }
+
+  if (onlyOneInput) {
+    const urls = await (0, _getUrlsFromOneFile.default)(input[0], {
+      exclude: (0, _getExclusion.default)(exclude),
+      extractFromQueryString
+    });
+    await (0, _saveFile.default)((0, _join.default)(Array.from(urls), '\n'), (0, _getOutput.default)(out));
+  }
+
+  if (hasMutipleInput) {
+    const urls = await (0, _getUrlsFromMultipleFiles.default)(input, {
+      exclude: (0, _getExclusion.default)(exclude),
+      extractFromQueryString
+    });
+    await (0, _saveFile.default)((0, _join.default)(urls, '\n'), (0, _getOutput.default)(out));
+  }
+};
+
 (async () => {
   try {
-    const cli = (0, _meow.default)(`
-      使用方式
-        $ get-urls <文件> <...> 选项 [...]
-
-      选项
-        --out, -o, 指定存储位置，默认：'process.cwd()'，包含文件名，将获取到的链接以文件的形式保存
-        --exclude, -f, 指定一个或多个过滤规则
-        --extractFromQueryString, 是否从找到的链接中提取作为查询参数内容链接
-        --version, -V, 查看版本号
-
-      示例
-        $ get-urls ./playlist.m3u -o ./test.txt -f '(s|jpg)'
-    `, {
-      flags: {
-        exclude: {
-          type: 'string',
-          alias: 'f'
-        },
-        out: {
-          type: 'string',
-          alias: 'o'
-        },
-        extractFromQueryString: {
-          type: 'boolean'
-        },
-        help: {
-          type: 'boolean',
-          alias: 'h'
-        },
-        version: {
-          type: 'boolean',
-          alias: 'V'
-        }
-      }
-    });
-    (0, _updateNotifier.default)({
-      pkg: cli.pkg
-    }).notify();
-    const {
-      input,
-      flags
-    } = cli;
-    const {
-      out,
-      exclude,
-      extractFromQueryString
-    } = flags;
-    const noInput = (0, _isEqual.default)((0, _size.default)(input), 0);
-    const onlyOneInput = (0, _isEqual.default)((0, _size.default)(input), 1);
-    const hasMutipleInput = (0, _size.default)(input) > 1;
-
-    if (noInput) {
-      throw Error('必须提供源文件');
-    }
-
-    if (onlyOneInput) {
-      const urls = await (0, _getUrlsFromOneFile.default)(input[0], {
-        exclude: (0, _getExclusion.default)(exclude),
-        extractFromQueryString
-      });
-      await (0, _saveFile.default)((0, _join.default)(Array.from(urls), '\n'), (0, _getOutput.default)(out));
-    }
-
-    if (hasMutipleInput) {
-      const urls = await (0, _getUrlsFromMultipleFiles.default)(input, {
-        exclude: (0, _getExclusion.default)(exclude),
-        extractFromQueryString
-      });
-      await (0, _saveFile.default)((0, _join.default)(urls, '\n'), (0, _getOutput.default)(out));
-    }
+    await start();
   } catch (err) {
     throw err;
   }
--- src/cli/index.js
+++ src/cli/index.js
@@ -14,86 +14,91 @@ import getOutput from './getOutput';
 
 // 待办： 支持直接在命令行界面中提供一段文本并从中获取所有链接
 // 待办： 若未提供 `out` 参数，则直接在命令行界面中输出结果
+
+const start = async () => {
+  const cli = meow(`
+    使用方式
+      $ get-urls <文件> <...> 选项 [...]
+
+    选项
+      --out, -o, 指定存储位置，默认：'process.cwd()'，包含文件名，将获取到的链接以文件的形式保存
+      --exclude, -f, 指定一个或多个过滤规则
+      --extractFromQueryString, 是否从找到的链接中提取作为查询参数内容链接
+      --version, -V, 查看版本号
+
+    示例
+      $ get-urls ./playlist.m3u -o ./test.txt -f '(s|jpg)'
+  `, {
+    flags: {
+      exclude: {
+        type: 'string',
+        alias: 'f',
+      },
+      out: {
+        type: 'string',
+        alias: 'o',
+      },
+      extractFromQueryString: {
+        type: 'boolean',
+      },
+      help: {
+        type: 'boolean',
+        alias: 'h',
+      },
+      version: {
+        type: 'boolean',
+        alias: 'V',
+      },
+    },
+  });
+
+  updateNotifier({ pkg: cli.pkg }).notify();
+
+  const { input, flags } = cli;
+  const { out, exclude, extractFromQueryString } = flags;
+
+  const noInput = isEqual(size(input), 0);
+  const onlyOneInput = isEqual(size(input), 1);
+  const hasMutipleInput = size(input) > 1;
+
+  if (noInput) {
+    throw Error('必须提供源文件');
+  }
+
+  if (onlyOneInput) {
+    const urls = await getUrlsFromOneFile(
+      input[0],
+      {
+        exclude: getExclusion(exclude),
+        extractFromQueryString,
+      },
+    );
+
+    await saveFile(
+      join(Array.from(urls), '\n'),
+      getOutput(out),
+    );
+  }
+
+  if (hasMutipleInput) {
+    const urls = await getUrlsFromMultipleFiles(
+      input,
+      {
+        exclude: getExclusion(exclude),
+        extractFromQueryString,
+      },
+    );
+
+    await saveFile(
+      join(urls, '\n'),
+      getOutput(out),
+    );
+  }
+};
+
 (async () => {
   try {
-    const cli = meow(`
-      使用方式
-        $ get-urls <文件> <...> 选项 [...]
-
-      选项
-        --out, -o, 指定存储位置，默认：'process.cwd()'，包含文件名，将获取到的链接以文件的形式保存
-        --exclude, -f, 指定一个或多个过滤规则
-        --extractFromQueryString, 是否从找到的链接中提取作为查询参数内容链接
-        --version, -V, 查看版本号
-
-      示例
-        $ get-urls ./playlist.m3u -o ./test.txt -f '(s|jpg)'
-    `, {
-      flags: {
-        exclude: {
-          type: 'string',
-          alias: 'f',
-        },
-        out: {
-          type: 'string',
-          alias: 'o',
-        },
-        extractFromQueryString: {
-          type: 'boolean',
-        },
-        help: {
-          type: 'boolean',
-          alias: 'h',
-        },
-        version: {
-          type: 'boolean',
-          alias: 'V',
-        },
-      },
-    });
-
-    updateNotifier({ pkg: cli.pkg }).notify();
-
-    const { input, flags } = cli;
-    const { out, exclude, extractFromQueryString } = flags;
-
-    const noInput = isEqual(size(input), 0);
-    const onlyOneInput = isEqual(size(input), 1);
-    const hasMutipleInput = size(input) > 1;
-
-    if (noInput) {
-      throw Error('必须提供源文件');
-    }
-
-    if (onlyOneInput) {
-      const urls = await getUrlsFromOneFile(
-        input[0],
-        {
-          exclude: getExclusion(exclude),
-          extractFromQueryString,
-        },
-      );
-
-      await saveFile(
-        join(Array.from(urls), '\n'),
-        getOutput(out),
-      );
-    }
-
-    if (hasMutipleInput) {
-      const urls = await getUrlsFromMultipleFiles(
-        input,
-        {
-          exclude: getExclusion(exclude),
-          extractFromQueryString,
-        },
-      );
-
-      await saveFile(
-        join(urls, '\n'),
-        getOutput(out),
-      );
-    }
+    await start();
   } catch (err) {
     throw err;
   }
